5252b2c225853c065aee9340e38c905bd0bcef3d
Nishihatak <nishihata1451@gmail.com>
2019-11-21 13:11:09 +0900

    gem,devise,haml,scss

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :items
+  has_many :comments
+  has_many :credit_cards
+  has_many :likes
+  has_many :liked_items, through: :likes, source: :item
+  has_many :rates
+  has_many :ratings, through: :rates, source: :rated_user
+end
