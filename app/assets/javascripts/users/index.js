9da9280950f0bbab6a35757a9b744552edeff2ba
tatsuya551 <seki0515ya@gmail.com>
2019-11-25 19:40:00 +0900

    user_mypage2

--- app/assets/javascripts/users/index.js
+++ app/assets/javascripts/users/index.js
@@ -1,25 +1,24 @@
 $(document).on('turbolinks:load', function () {
-  let newsTabs = $(".information__tabs__btn");
-  newsTabsAry = Array.prototype.slice.call(newsTabs);
+  let news_tabs = $(".information__tabs__btn");
   
   function newsTabSwitch () {
     $(".active").removeClass("active");
     $(this).addClass("active");
-    const index = newsTabs.index(this);
+    const index = news_tabs.index(this);
     $(".content__list").eq(index).addClass("active")
   }
   
-  let itemTabs = $(".buy-item__tabs__btn");
-  itemTabsAry = Array.prototype.slice.call(itemTabs);
+  
+  let item_tabs = $(".buy-item__tabs__btn");
   
   function itemTabSwitch () {
     $(".active-2").removeClass("active-2");
     $(this).addClass("active-2");
-    const index = itemTabs.index(this);
+    const index = item_tabs.index(this);
     $(".buy-item__content__list").eq(index).addClass("active-2")
   }
   
-  newsTabs.click(newsTabSwitch);
-  itemTabs.click(itemTabSwitch);
+  news_tabs.click(newsTabSwitch);
+  item_tabs.click(itemTabSwitch);
   
 });
--- app/assets/javascripts/users/side_bar.js
+++ app/assets/javascripts/users/side_bar.js
@@ -1,11 +1,11 @@
 $(document).on('turbolinks:load', function () {
   $('.mypage-nav-list-item').hover(
     function(){
-      $(this).find('.icon-arrow-right').css({color: '#333'}).animate({right: "10px"}, 20);
-      $(this).css({background: '#f8f8f8'})
+      $(this).find('.icon-arrow-right').css({color: '#333'}).animate({right: "10px"}, 50);
+      $(this).css({background: 'rgb(250, 250, 250)'})
     },
     function(){
-      $(this).find('.icon-arrow-right').css({color: '#ccc'}).animate({right: "16px"}, 20);
+      $(this).find('.icon-arrow-right').css({color: 'rgb(230, 230, 230)'}).animate({right: "16px"}, 50);
       $(this).css({background: '#fff'})
   });
 });
