1dccf9b9ac90630ed03021c362e5fca1e002ebcd
Nishihatak <nishihata1451@gmail.com>
2019-11-27 19:58:29 +0900

    ウィザード機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   
+  resources :signup do
+    collection do
+      get 'step1'
+    end
+  end
+
   root "users#index"
   resources :users, only: [:index]
 end
