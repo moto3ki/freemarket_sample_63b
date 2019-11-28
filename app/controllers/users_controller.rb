0bb9409c3811ce030659aec6f5c7d199c239936a
tatsuya551 <seki0515ya@gmail.com>
2019-11-28 13:13:51 +0900

    user_index_edit_serverside

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,26 @@
 class UsersController < ApplicationController
 
   def index
-    
+
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
 
   end
 
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname, :profile)
+  end
 
 
 end
