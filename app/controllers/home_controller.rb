3b93a1dd84576cbeb0b0d55b2cb9e6e848746b21
tatsuya551 <seki0515ya@gmail.com>
2019-11-29 12:28:05 +0900

    top_page_serverside

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
+  
   def index
-    
+    @items = Item.all.order("created_at DESC").limit(10)
+    @item_images = ItemImage.all.order("created_at DESC").limit(10)
   end
+  
 end
