00472daecb1941c4d5a245c704aa99ce320223ab
moto3ki <moto3ki@gmail.com>
2019-11-29 14:34:13 +0900

    credit_db_syuusei

--- db/migrate/20191129040116_create_credit_cards.rb
+++ db/migrate/20191129040116_create_credit_cards.rb
@@ -2,8 +2,8 @@ class CreateCreditCards < ActiveRecord::Migration[5.2]
   def change
     create_table :credit_cards do |t|
       t.integer  :user_id,               null: false, foreign_key: true
-      t.string  :customer_id,               null: false, foreign_key: true
-      t.string  :card_id,               null: false, foreign_key: true
+      t.string  :customer_id,               null: false
+      t.string  :card_id,               null: false
 
       t.timestamps  null: false
     end
