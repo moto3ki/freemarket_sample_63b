407f836443a3894f907f39ec57a2893a49f51faf
moto3ki <moto3ki@gmail.com>
2019-12-09 20:58:22 +0900

    payjp_purchase

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,23 +1,48 @@
 class PurchasesController < ApplicationController
-  def new
-    @item = Item.find(params[:item_id])
+
+  require 'payjp'
+
+  def  done
+    
   end
 
   def create
-    
     item = Item.find(params[:item_id])
+    credit_card = CreditCard.find_by(user_id: current_user.id)
+    Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+    Payjp::Charge.create(
+      amount: item.price,
+      customer: credit_card.customer_id, 
+      currency: 'jpy'
+    )
+    
     item.status = 1
 
     purchase = Purchase.new(item_params)
     if item.save && purchase.save
       redirect_to root_path
     else
-      redirect_to new_item_purchase_path(item)
+      redirect_to action: 'done'
     end
   end
+
+  def new
+    credit_card = CreditCard.find_by(user_id: current_user.id)
+    if credit_card.blank?
+      redirect_to controller: "credit_cards", action: "new"
+    else
+      Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
+      customer = Payjp::Customer.retrieve(credit_card.customer_id)    
+      @default_card_information = customer.cards.retrieve(credit_card.card_id)
+
+      @item = Item.find(params[:item_id])
+    end
+  end
+
 
   private
   def item_params
     params.permit(:item_id).merge(user_id: current_user.id)
   end
+  
 end
