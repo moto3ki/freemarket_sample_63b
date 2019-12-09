e0984a1619b63185bb7e473203bc060934e90560
matsu <44367201+pinecode1@users.noreply.github.com>
2019-12-09 09:58:36 +0900

    Add: breadcrumbs-list

---
+++ config/breadcrumbs.rb
@@ -0,0 +1,63 @@
+crumb :root do
+  link "メルカリ", root_path
+end
+
+crumb :mypage do
+  link "マイページ", users_path
+  parent :root
+end
+
+crumb :selling_items do
+  link "出品した商品 - 出品中", selling_items_users_path
+  parent :mypage
+end
+
+crumb :sold_items do
+  link "出品した商品 - 売却済み", sold_items_users_path
+  parent :mypage
+end
+
+crumb :profile do
+  link "プロフィール", edit_user_path
+  parent :mypage
+end
+
+crumb :credit_cards do
+  link "支払い方法", new_credit_card_path
+  parent :mypage
+end
+
+crumb :real_address do
+  link "本人情報の登録", new_user_real_address_path
+  parent :mypage
+end
+
+crumb :logout do
+  link "ログアウト", logout_users_path
+  parent :mypage
+end
+
+# crumb :projects do
+#   link "Projects", projects_path
+# end
+
+# crumb :project do |project|
+#   link project.name, project_path(project)
+#   parent :projects
+# end
+
+# crumb :project_issues do |project|
+#   link "Issues", project_issues_path(project)
+#   parent :project, project
+# end
+
+# crumb :issue do |issue|
+#   link issue.title, issue_path(issue)
+#   parent :project_issues, issue.project
+# end
+
+# If you want to split your breadcrumbs configuration over multiple files, you
+# can create a folder named `config/breadcrumbs` and put your configuration
+# files there. All *.rb files (e.g. `frontend.rb` or `products.rb`) in that
+# folder are loaded and reloaded automatically when you change them, just like
+# this file (`config/breadcrumbs.rb`).
